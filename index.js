de8cc347bf417b68aa7b717e2ce42577f432cd65
Oisin Tohak <52585913+oisintohak@users.noreply.github.com>
2020-04-19 23:50:34 +0100

    html layout

---
+++ index.js
@@ -0,0 +1,7 @@
+const DOMselectors = {
+    calculatorWrapper : document.querySelector('.calculator-wrapper'),
+    clearButton: document.querySelector('.clear'),
+    viewer: document.querySelector('.viewer'),
+    numButtons: Array.from(document.querySelectorAll('.num')),
+    
+}
