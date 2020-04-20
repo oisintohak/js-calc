66dd3a15289786e36417c889a87399a8b2480294
Oisin Tohak <52585913+oisintohak@users.noreply.github.com>
2020-04-20 23:27:31 +0100

    building

--- index.js
+++ index.js
@@ -3,5 +3,30 @@ const DOMselectors = {
     clearButton: document.querySelector('.clear'),
     viewer: document.querySelector('.viewer'),
     numButtons: Array.from(document.querySelectorAll('.num')),
-    
+    opsButtons: document.querySelectorAll('.ops')
 }
+
+const state = {
+}
+
+function updateView (value) {
+    if (value.id = 'clear') {
+        DOMselectors.viewer.innerHTML = 0;
+    }
+}
+
+// UI CONTROLLER
+const UIController = {
+}
+
+const globalController = {
+
+}
+
+const clearBut = document.querySelector('.clear');
+
+clearBut.addEventListener("click", updateView);
+//EVENT LISTENERS:
+
+//NUMBERS:
+// DOMselectors.clearButton.addEventListener('click', updateView(0));
