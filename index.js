b1500c85c4b1e618a81f5645ad9ce8f7ce5ffe0c
Oisin Tohak <52585913+oisintohak@users.noreply.github.com>
2020-04-22 00:34:59 +0100

    building calc

--- index.js
+++ index.js
@@ -11,24 +11,25 @@ const state = {
         //combine the numbers into a value
     //user presses an M/D/A/S:
         //apply the operator if there was a previous value
-
-    viewerString: '',
-    currentValue: 0,
-    previousValue: '',
-    currentOperator: '',
-    previousOperator: '',
-    result: 0
-    
-
+    clearState: () => {
+        state.viewerString = '0';
+        state.currentValue = '';
+        state.previousValue = '';
+        state.currentOperator = '';
+        state.previousOperator = '';
+        state.result = 0;
+    }
 }
 
+state.clearState();
 
 
 // UI CONTROLLER
 const UIController = {
     
-    updateView: (value) => {
-        DOMselectors.viewer.innerHTML = value;
+    updateView: () => {
+        DOMselectors.viewer.innerHTML = state.viewerString;
+        console.log(state);
     }
 }
 
@@ -36,10 +37,34 @@ const UIController = {
 
 const inputController = {
     getInputClick: (event) => {
-        console.log(event);
+
         //CLEAR:
-        if (event.id = 'clear') {
-            UIController.updateView('0');
+        if (event.target.id == 'clear') {
+            state.clearState();
+            UIController.updateView();
+        }
+
+        // EQUALS:
+        if (event.target.id == 'equals') {
+            //CALCULATE RESULT:
+            state.viewerString = calcController.calculate();
+            //UPDATE UI
+            UIController.updateView();
+            // CLEAR previous value
+            state.currentValue = state.viewerString;
+            state.previousValue = '';
+        }
+        //NUMBERS:
+        if (event.target.classList.contains('num')) {
+            //add digit to current value
+            state.currentValue += `${event.target.dataset.num}`;
+
+            //update viewerstring
+            state.viewerString = state.currentValue;
+
+            // update UI
+            UIController.updateView();
+            
         }
 
         //OPERATOR (M/D/A/S):
@@ -50,7 +75,7 @@ const inputController = {
                 state.previousValue = state.currentValue;                
                 
                 //set current value to 0:
-                state.currentValue = 0;
+                state.currentValue = '';
                 
                 //record operator
                 state.currentOperator = event.target.dataset.ops;
@@ -59,18 +84,13 @@ const inputController = {
                 // calculate result using 2 values & previous operator
                 // asign result to previous value
                 state.previousValue = calcController.calculate();
+                state.viewerString = state.previousValue;
+
+                //record operator
+                state.currentOperator = event.target.dataset.ops;
             }
         }
-
-        if (state.previousValue && event.target.classList.contains('ops')) {
-
-        }
         
-        //NUMBERS:
-        if (event.target.classList.contains('num')) {
-            state.currentValue += `${event.target.dataset.num}`;
-            
-        }
     }
 }
 
@@ -82,30 +102,32 @@ const inputController = {
 const calcController = {
     calculate : () => {
         let result;
+        console.log(state.previousValue, state.currentValue);
         let val1 = parseFloat(state.currentValue);
         let val2 = parseFloat(state.previousValue);
+        console.log(val1, val2);
         // MULTIPLY:
-        if(state.currentOperator = 'multiplication') {
+        if(state.currentOperator == 'multiplication') {
             result = val2 * val1;
         }
         //DIVIDE:
-        if(state.currentOperator = 'division') {
+        if(state.currentOperator == 'division') {
             result = val2 / val1;
         }
         //ADD:
-        if(state.currentOperator = 'addition') {
+        if(state.currentOperator == 'addition') {
             result = val2 + val1;
         }
         //SUBTRACT:
-        if(state.currentOperator = 'subtraction') {
+        if(state.currentOperator == 'subtraction') {
             result = val2 - val1;
         }
-        return toString(result);
+        console.log(result);
+        return result.toString(10);
 
     }
 }
 
-// DOMselectors.clearButton.addEventListener("click", inputController.getInput);
 //EVENT LISTENERS:
 
 //NUMBERS:
